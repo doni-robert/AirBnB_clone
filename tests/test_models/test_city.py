c99f509fb392f239abd81298066d05dd7e0367ec
doni-robert <irobertndungu@gmail.com>
2023-05-21 13:23:55 +0300

    add test

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -31,6 +31,12 @@ class TestCity(unittest.TestCase):
         self.assertTrue(hasattr(self.new, 'created_at'))
         self.assertTrue(hasattr(self.new, 'updated_at'))
 
+    ef test_pep8_conformance_city(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/city.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
 if __name__ == '__main__':
     unittest.main()
